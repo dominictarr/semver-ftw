807bacb7398eef107d7123630b2db737a91c98ba
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-21 16:01:10 +1300

    add fork me ribbon

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@
   echo '<!doctype html>'
   echo '<html><head>'
   echo '<link rel=stylesheet href=./style.css>'
+  echo '<a href="https://github.com/you">'
+  echo '<img style="position: absolute; top: 0; left: 0; border: 0;"'\
+  'src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"'\
+  ' alt="Fork me on GitHub"></a>'
+
   echo '<script src=http://rumoursdb.com/examples/css-colab/css-colab.js></script>'
   echo '<script>CssColab("semver-ftw")</script>'
 
