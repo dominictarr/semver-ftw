749a950ccc0f52e7a96e0f81666d93c965b0767c
Dominic Tarr <dominic.tarr@gmail.com>
2015-05-31 20:56:55 +0200

    fix github ribbon

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
   echo '<!doctype html>'
   echo '<html><head>'
   echo '<link rel=stylesheet href=./style.css>'
-  echo '<a href="https://github.com/you">'
+  echo '<a href="https://github.com/dominictarr/semver-ftw">'
   echo '<img style="position: absolute; top: 0; left: 0; border: 0;"'\
   'src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"'\
   ' alt="Fork me on GitHub"></a>'
